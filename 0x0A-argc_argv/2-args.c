beabf4265c4ff1dac878def1b5fc6a3903e28aba
“ezekenneth” <ezekenny91@gmail.com>
2023-04-07 08:29:22 -0700

    l

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,41 +1,19 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
 /**
- * main - adds positive numbers
+ * main - prints all the arguments it receives
  * @argv: array of pointers to strings
  * @argc: argument count
- *
- * Return: 0 or 1
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
-	int array[] = {25, 10, 5, 2, 1};
-	int i, cents;
-	int count = 0;
+	int i = 0;
 
-	if (argc != 2)
+	while (i < argc)
 	{
-		printf("Error\n");
-		return (1);
-	}
-	cents = atoi(argv[1]);
-	if (cents < 0)
-	{
-		printf("0\n");
-	}
-	for (i = 0; i < 5; i++)
-	{
-		if (cents >= array[i])
-		{
-			count += (cents / array[i]);
-			cents = cents % array[i];
-		}
-		if (cents == 0)
-		{
-			printf("%d\n", count);
-			break;
-		}
+		printf("%s\n", argv[i]);
+		i++;
 	}
 	return (0);
 }
