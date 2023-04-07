19de6944e52a7df7f73d7763d43ef60917ec3a68
“ezekenneth” <ezekenny91@gmail.com>
2023-04-07 08:12:39 -0700

    l

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,35 +1,13 @@
+#include "holberton.h"
 #include <stdio.h>
-		
-#include "main.h"
-		
-
-		
 /**
-		
- * main - print the number of arguments passed to the program
-		
- * @argc: number of arguments
-		
- * @argv: array of arguments
-		
- *
-		
- * Return: Always 0 (Success)
-		
+ * main - prints a number, followed by a new line
+ * @argv: array of pointers to strings
+ * @argc: argument count
+ * Return: 0
  */
-		
-
-		
-int main(int argc, char *argv[])
-		
+int main(int argc, __attribute__ ((unused)) char *argv[])
 {
-		
-(void) argv; /*Ignore argv*/
-		
 	printf("%d\n", argc - 1);
-		
-
-		
 	return (0);
-		
 }
