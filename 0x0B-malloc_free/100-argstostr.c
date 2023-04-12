a491f3a6026fa14a8957f9809cf0b10f34299273
“ezekenneth” <ezekenny91@gmail.com>
2023-04-11 23:51:38 -0700

    l

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -5,7 +5,6 @@
 * *argstostr - concatenates all the arguments of the program
 * @ac: number of arguments
 * @av: array of arguments
-*
 * Return: Pointer to the new string (Success), NULL (Error)
 */
 char *argstostr(int ac, char **av)
@@ -33,13 +32,13 @@ char *argstostr(int ac, char **av)
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
-		{
-			str[k] = av[i][j];
-			k++;
-		}
-		str[k] = '\n';
+	{
+		str[k] = av[i][j];
 		k++;
 	}
+	str[k] = '\n';
+	k++;
+	}
 	
-	return (str);
+return (str);
 }
