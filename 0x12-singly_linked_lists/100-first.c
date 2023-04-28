110c597a04381fe9747818f7d40a5e7b70e14acc
“ezekenneth” <ezekenny91@gmail.com>
2023-04-28 10:11:17 -0700

    l

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -13,6 +13,6 @@ void __attribute__((constructor)) hare(void);
  */
 void hare(void)
 {
-	printf("i love you! so, you must accept,\n"
-	       "I bore my house upon my head!\n");
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
 }
